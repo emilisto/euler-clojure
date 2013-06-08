3c5260694b7521a5432880c60d090f2dd783562b
Emil Stenqvist <emsten@gmail.com>
2013-06-08 23:59:46 +0200

    Solved problem #2

--- src/euler/p2.clj
+++ src/euler/p2.clj
@@ -6,18 +6,18 @@
 
 (test/with-test
   (defn fibonacci-til [max]
-    (if (= max 0)
+    "Gives the Fibonacci sequence whose terms go up to, but not equal to, max"
+    (if (<= max 1)
       '(0)
-      (if (= max 1)
-        '(0 1)
-        (loop [ seq '(0 1) ]
-          (if (< (last seq) max)
-            (let [term (+ (last seq) (llast seq))]
-              (recur (concat seq (list term)) ))
+      (loop [ seq '(0 1) ]
+        (let [term (+ (last seq) (llast seq))]
+          (if (< term max)
+            (recur (concat seq (list term)) )
             seq)))))
+  
   (test/is ( = (fibonacci-til 1) '(0 1)))
   (test/is ( = (fibonacci-til 2) '(0 1 1 2)))
-  (test/is ( = (fibonacci-til 13) '(0 1 1 2 3 5 8 13))))
+  (test/is ( = (fibonacci-til 15) '(0 1 1 2 3 5 8 13))))
 
 (defn euler-p2 []
-  (fibonacci-til 4000000))
+  (reduce + (filter even? (fibonacci-til 4000000))))
